bbfdea3877d208de19c08f35445d7c9828054c1f
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-09-13 03:26:49 +0300

    mm

--- web_static/landing.js
+++ web_static/landing.js
@@ -8,6 +8,37 @@ const twitter = document.getElementById('twitter');
 const ig = document.getElementById('ig');
 
 join.addEventListener('click', () => {
-    window.location.href = "login.html";
+  window.location.href = 'login.html';
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const openPopupButtons = document.querySelectorAll('.footer-links a');
+  const closePopupButtons = document.querySelectorAll('.popup .close');
+
+  openPopupButtons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      event.preventDefault();
+      const popupId = `popup-${this.id}`;
+      const popup = document.getElementById(popupId);
+      if (popup) {
+        popup.style.display = 'block';
+      }
+    });
+  });
+
+  closePopupButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const popupId = button.getAttribute('data-close');
+      const popup = document.getElementById(popupId);
+      if (popup) {
+        popup.style.display = 'none';
+      }
+    });
+  });
+
+  window.addEventListener('click', (event) => {
+    if (event.target.classList.contains('popup')) {
+      event.target.style.display = 'none';
+    }
+  });
+});
